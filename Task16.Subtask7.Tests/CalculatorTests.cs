47acf11ca40e82517178f1271da95fe5955cb62e
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-03-16 15:53:14 +0500

    Добавьте файлы проекта.

---
+++ Task16.Subtask7.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16.Subtask7.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 30) == 40);
+        }
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(100, 10) == 90);
+        }
+        [Test]
+        public void Multiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Multiplication(3, 5) == 15);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(500, 10) == 50);
+        }
+        [Test]
+        public void DivisionByZero_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(10, 0));
+        }
+
+    }
+}
